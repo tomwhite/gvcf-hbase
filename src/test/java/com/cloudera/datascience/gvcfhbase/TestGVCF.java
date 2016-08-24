1c56d598fe0dfe57ccd8bac7deec6f2cbca13be0
Tom White <tom@cloudera.com>
2016-08-24 14:23:19 +0100

    Bulk insert some data into HBase.

--- src/test/java/com/cloudera/datascience/gvcfhbase/TestGVCF.java
+++ src/test/java/com/cloudera/datascience/gvcfhbase/TestGVCF.java
@@ -1,10 +1,19 @@
 package com.cloudera.datascience.gvcfhbase;
 
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
-import org.apache.hadoop.hbase.MiniHBaseCluster;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.spark.JavaHBaseContext;
+import org.apache.hadoop.hbase.spark.example.hbasecontext.JavaHBaseBulkPutExample;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -30,8 +39,30 @@ public class TestGVCF {
     HTable table = testUtil.createTable(tableName, Bytes.toBytes("s")); // s for sample
 
     for (TableName n : testUtil.getHBaseAdmin().listTableNames()) {
-      System.out.println(n.getNameAsString());
+      System.out.println("tw: " + n.getNameAsString());
     }
+
+    // create an RDD
+    SparkConf sparkConf = new SparkConf()
+        .setMaster("local")
+        .setAppName(getClass().getSimpleName() + tableName)
+        .set("spark.io.compression.codec", "lzf");
+    JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+    List<String> list = ImmutableList.of("1,s,a,0|0", "2,s,a,1|1");
+    JavaRDD<String> rdd = jsc.parallelize(list);
+
+    // bulk insert into HBase
+    Configuration conf = testUtil.getConfiguration();
+    JavaHBaseContext hbaseContext = new JavaHBaseContext(jsc, conf);
+    hbaseContext.bulkPut(rdd, tableName, (Function<String, Put>) v -> {
+      String[] cells = v.split(",");
+      Put put = new Put(Bytes.toBytes(cells[0]));
+      put.addColumn(Bytes.toBytes(cells[1]), Bytes.toBytes(cells[2]),
+          Bytes.toBytes(cells[3]));
+      return put;
+    });
+
+    // TODO: read back data
 
     testUtil.deleteTable(tableName);
   }
