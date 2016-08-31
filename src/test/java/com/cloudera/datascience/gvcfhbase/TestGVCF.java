299eab982b9db2a93748498ab1fff45619e2b8e9
Tom White <tom@cloudera.com>
2016-08-31 09:52:25 +0100

    Start of splits working...

--- src/test/java/com/cloudera/datascience/gvcfhbase/TestGVCF.java
+++ src/test/java/com/cloudera/datascience/gvcfhbase/TestGVCF.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.BufferedMutator;
+import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
@@ -23,6 +25,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -49,7 +52,9 @@ public class TestGVCF implements Serializable {
   @Test
   public void test() throws Exception {
     TableName tableName = TableName.valueOf("gvcf");
-    HTable table = testUtil.createTable(tableName, SAMPLE_COLUMN_FAMILY);
+    byte[][] columnFamilies = new byte[][] { SAMPLE_COLUMN_FAMILY };
+    byte[][] splitKeys = new byte[][] { Bytes.toBytes(5) };
+    HTable table = testUtil.createTable(tableName, columnFamilies, splitKeys);
 
     for (TableName n : testUtil.getHBaseAdmin().listTableNames()) {
       System.out.println("tw: " + n.getNameAsString());
@@ -97,33 +102,81 @@ public class TestGVCF implements Serializable {
         "8,1|1(end=8),0|0(end=8)");
     assertEquals(expectedAllPositions, allPositions);
 
-    // Scan over variants only
-    List<String> allVariants = scan(tableName, hbaseContext, false, TestGVCF::process).collect();
-    //allVariants.forEach(System.out::println);
-    List<String> expectedAllVariants = ImmutableList.of(
-        "1,0|0(end=1),0|1(end=3)",
-        "7,N/A(end=7),0|0(end=8)",
-        "8,1|1(end=8),0|0(end=8)");
-    assertEquals(expectedAllVariants, allVariants);
+//    // Scan over variants only
+//    List<String> allVariants = scan(tableName, hbaseContext, false, TestGVCF::process).collect();
+//    //allVariants.forEach(System.out::println);
+//    List<String> expectedAllVariants = ImmutableList.of(
+//        "1,0|0(end=1),0|1(end=3)",
+//        "7,N/A(end=7),0|0(end=8)",
+//        "8,1|1(end=8),0|0(end=8)");
+//    assertEquals(expectedAllVariants, allVariants);
 
     testUtil.deleteTable(tableName);
   }
 
+  /**
+   * Like {@link JavaHBaseContext#bulkPut(JavaRDD, TableName, Function)}, but allows
+   * multiple puts per entry in the RDD.
+   * @param <T>
+   * @param hbaseContext
+   * @param rdd
+   * @param tableName
+   * @param f
+   */
+  private static <T> void bulkPut(JavaHBaseContext hbaseContext, JavaRDD<T> rdd,
+      TableName tableName, FlatMapFunction<T, Put> f) {
+    final byte[] tName = tableName.getName();
+    hbaseContext.foreachPartition(rdd, (VoidFunction<Tuple2<Iterator<T>, Connection>>)
+        tuple -> {
+          Connection connection = tuple._2();
+          BufferedMutator m = connection.getBufferedMutator(TableName.valueOf(tName));
+          try {
+            for (Iterator<T> i = tuple._1(); i.hasNext(); ) {
+              for (Put put : f.call(i.next())) {
+                m.mutate(put);
+              }
+            }
+          } finally {
+            m.flush();
+            m.close();
+          }
+        });
+  }
+
   public void put(JavaRDD<VariantLite> rdd, TableName tableName, JavaHBaseContext
       hbaseContext, int splitSize) {
-    // TODO: how to break variants into two that span the split boundary? (may need to
-    // write own bulkPut with a flatmap over puts
     // TODO: can we use bulkLoad for efficiency (need to port interface to Java)
-    hbaseContext.bulkPut(rdd, tableName, (Function<VariantLite, Put>) v -> {
-      Put put = new Put(Bytes.toBytes(v.getStart()));
-      // note that we only store one genotype here as we expect to load single sample
-      // gvcf files
-      byte[] qualifier = Bytes.toBytes(v.getGenotype().getSampleIndex());
-      String val = v.getEnd() + "," + v.getGenotype().getValue(); // poor encoding!
-      byte[] value = Bytes.toBytes(val);
-      put.addColumn(SAMPLE_COLUMN_FAMILY, qualifier, value);
-      return put;
+    bulkPut(hbaseContext, rdd, tableName, (FlatMapFunction<VariantLite, Put>) v -> {
+      List<Put> puts = null;
+      int start = v.getStart();
+      int end = v.getEnd();
+      int startSplit = (start - 1) / splitSize; // start and end are 1-based like VCF
+      int endSplit = (end - 1) / splitSize;
+      if (startSplit == endSplit) {
+        puts = ImmutableList.of(encodePut(start, end, v.getGenotype()));
+      } else {
+        int midStart = (startSplit + 1) * splitSize + 1;
+        int midEnd = midStart - 1;
+        puts = ImmutableList.of(encodePut(start, midEnd, v.getGenotype()),
+            encodePut(midStart, end, v.getGenotype()));
+      }
+      return puts;
     });
+  }
+
+  private Put encodePut(int start, int end, GenotypeLite genotype) {
+    Put put = new Put(Bytes.toBytes(start));
+    // note that we only store one genotype here as we expect to load single sample
+    // gvcf files
+    byte[] qualifier = Bytes.toBytes(genotype.getSampleIndex());
+    String val = end + "," + genotype.getValue(); // poor encoding!
+    byte[] value = Bytes.toBytes(val);
+    put.addColumn(SAMPLE_COLUMN_FAMILY, qualifier, value);
+    System.out.println("put: " + genotype.getSampleIndex() + ":" + start + "(" + end +
+        ") "
+        + genotype
+        .getValue());
+    return put;
   }
 
   public <T> JavaRDD<T> scan(TableName tableName, JavaHBaseContext
@@ -142,6 +195,8 @@ public class TestGVCF implements Serializable {
             if (numSamples == -1) { // determine number of samples from first row,
               // since they all have an entry there TODO: use column family metadata here
               numSamples = result.listCells().size();
+              System.out.println("tw: num samples : " + numSamples);
+              System.out.println("tw: row key: " + Bytes.toInt(result.getRow()));
               variantsBySampleIndex = Arrays.asList(new VariantLite[numSamples]);
             }
             int start = Bytes.toInt(result.getRow());
