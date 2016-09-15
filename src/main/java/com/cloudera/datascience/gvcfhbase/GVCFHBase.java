9b1f3ac1cb66da2c5a1f9fd1b89faaf3d04b76c3
Tom White <tom@cloudera.com>
2016-09-15 15:13:01 +0100

    f

--- src/main/java/com/cloudera/datascience/gvcfhbase/GVCFHBase.java
+++ src/main/java/com/cloudera/datascience/gvcfhbase/GVCFHBase.java
@@ -2,8 +2,6 @@ package com.cloudera.datascience.gvcfhbase;
 
 import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.ImmutableList;
-import htsjdk.samtools.util.IterableOnceIterator;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
